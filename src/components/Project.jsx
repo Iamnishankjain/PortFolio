0f7f4929120aeb413161c0524703ea86813b422a
Iamnishankjain <jnishank123@gmail.com>
2025-07-22 23:49:43 +0530

    responsive

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ function Project() {
         {
             projects.map((project)=>(
                 <div key={project.key} className='my-3 col-sm-6 col-md-4  col-lg-3 mx-3' data-aos="flip-right" data-aos-duration="500">
-                    <div className="card bg-dark text-light project-card" style={{width: "18rem"}}>
+                    <div className="card bg-dark text-light project-card" >
                         <div className="Project-img d-flex justify-content-center align-item-center p-3"><img src={`/assets/project/${project.imageSrc}`} className="card-img-top project-imgtag" alt="..." /></div>
                         <div className="card-body text-center">
                             <h5 className="card-title">{project.title}</h5>
