23e7453f21ad9d8be085587c01b7d67fa0d37740
Iamnishankjain <jnishank123@gmail.com>
2025-08-11 23:24:11 +0530

    update styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,24 +6,27 @@ function Project() {
     <>
       <div className="container project my-3">
         <h1 className='heading'>Projects</h1>
-
-        {/* ✅ Grid wrapper instead of Bootstrap's row/col */}
-        <div className="project-grid" id='project'>
+        <div className="row d-flex justify-content-center align-items-center" id='project'>
           {
             projects.map((project) => (
-              <div key={project.key} data-aos="flip-right" data-aos-duration="500">
-                <div className="card bg-dark text-light project-card">
-                  <div className="Project-img d-flex justify-content-center align-item-center p-3">
+              <div
+                key={project.key}
+                className='my-3 col-sm-6 col-md-4 col-lg-3 mx-3'
+                data-aos="flip-right"
+                data-aos-duration="500"
+              >
+                <div className="card bg-dark text-light project-card h-100 d-flex flex-column">
+                  <div className="Project-img d-flex justify-content-center align-items-center p-3">
                     <img
                       src={`/assets/project/${project.imageSrc}`}
                       className="card-img-top project-imgtag"
                       alt={project.title}
                     />
                   </div>
-                  <div className="card-body text-center">
+                  <div className="card-body d-flex flex-column">
                     <h5 className="card-title">{project.title}</h5>
-                    <p className="card-text">{project.description}</p>
-                    <a href={project.go} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Go</a>
+                    <p className="card-text flex-grow-1">{project.description}</p>
+                    <a href={project.go} target="_blank" rel="noopener noreferrer" className="btn btn-primary mt-auto">Go</a>
                   </div>
                 </div>
               </div>
